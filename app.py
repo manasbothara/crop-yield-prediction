c1569f374d35b519c16f68ff618a683efdc86235
Manas Bothara <manasbothara@gmail.com>
2023-01-29 14:44:15 +0530

    web app is ready

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 app=Flask(__name__)
 model = pickle.load(open('croppredict.pkl', 'rb'))
-pipeline = pickle.load(open('pipeline.pkl', 'rb'))
+# pipeline = pickle.load(open('pipeline.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -17,10 +17,19 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data = pipeline.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data = (np.array(list(data.values())).reshape(1,-1))
     output=model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+        data=[float(x) for x in request.form.values()]
+        final_input=(np.array(data).reshape(1,-1))
+        print(final_input)
+        output = model.predict(final_input)[0]
+        return render_template("home.html", prediction_text="the crop that can be grown is {}".format(output))
+
 
 if __name__=="__main__":
         app.run(debug=True)
